bcb919db22c021e2d79bd02bab779d9fd026699e
PRASHANT PRAKASH <155098714+prashant091993@users.noreply.github.com>
2025-01-04 23:12:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,7 +88,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "devopsbyrahamterraserverbucket9988oo9988"
+  bucket = "demoprojectofterraform"
 }
 
 resource "aws_iam_user" "seven" {
