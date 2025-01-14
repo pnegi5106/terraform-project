cc9d40c9b0218c2a1abf1fc8eead921d68a4f835
pnegi5106 <pnegi5106@gmail.com>
2025-01-14 23:24:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 
 provider "aws" {
-region = "ap-southeast-1"
+region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-07b69f62c1d38b012"
   instance_type   = "t2.micro"
-  key_name        = "abc"
+  key_name        = "terraformkey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-south-1a"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -18,16 +18,16 @@ chkconfig httpd on
 echo "hai all this is my app created by terraform infrastructurte by prashant sir server-1" > /var/www/html/index.html
 EOF
   tags = {
-    Name = "web-server-1"
+    Name = "T-web-server-1"
   }
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-07b69f62c1d38b012"
   instance_type   = "t2.micro"
-  key_name        = "abc"
+  key_name        = "terraformkey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "ap-south-1b"
   user_data       = <<EOF
 #!/bin/bash
 sudo -i
@@ -37,29 +37,29 @@ chkconfig httpd on
 echo "hai all this is my website created by terraform infrastructurte by prashant sir server-2" > /var/www/html/index.html
 EOF
   tags = {
-    Name = "web-server-2"
+    Name = "T-web-server-2"
   }
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-07b69f62c1d38b012"
   instance_type   = "t2.micro"
-  key_name        = "abc"
+  key_name        = "terraformkey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1a"
+  availability_zone = "ap-south-1a"
   tags = {
-    Name = "app-server-1"
+    Name = "t-app-server-1"
   }
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-06018068a18569ff2"
+  ami             = "ami-07b69f62c1d38b012"
   instance_type   = "t2.micro"
-  key_name        = "abc"
+  key_name        = "terraformkey"
   vpc_security_group_ids = [aws_security_group.five.id]
-  availability_zone = "ap-southeast-1b"
+  availability_zone = "ap-south-1b"
   tags = {
-    Name = "app-server-2"
+    Name = "t-app-server-2"
   }
 }
 
@@ -103,7 +103,7 @@ default = ["user1", "user2", "user3", "user4"]
 }
 
 resource "aws_ebs_volume" "eight" {
- availability_zone = "ap-southeast-1a"
+ availability_zone = "ap-south-1a"
   size = 40
   tags = {
     Name = "ebs-001"
